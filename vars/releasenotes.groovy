b622a62e8744a0bb34730695ac2b9676b813877c
Chris Behrens <cbehrens@gmail.com>
2020-03-04 15:40:48 -0600

    Added date and time

--- vars/releasenotes.groovy
+++ vars/releasenotes.groovy
@@ -1,22 +1,28 @@
-import java.io.File;
 import groovy.io.FileType;
+import java.io.File;
+import java.text.SimpleDateFormat
 
 @NonCPS
 def call(Map config=[:]){
-    def dir = new File(pwd());
+	def dir = new File(pwd());
     
-    new File(dir.path + '\\releasenotes.txt').withWriter('utf-8') { 
-        writer -> 
-            dir.eachFileRecurse(FileType.ANY){ file ->
-                if (file.isDirectory()){
-                    writer.writeLine(file.name);            
-                }
-                else
-                {
-                    writer.writeLine('\t' + file.name + '\t' + file.length());
-                }
-        } 
-    }
+	new File(dir.path + '\\releasenotes.txt').withWriter('utf-8') 
+	{ 
+		writer -> 
+	    dir.eachFileRecurse(FileType.ANY){ file ->
+		if (file.isDirectory()){
+		    writer.writeLine(file.name);            
+		}
+		else
+		{
+		    writer.writeLine('\t' + file.name + '\t' + file.length());
+		}
+		} 
+	}
+    
+	def now = new Date();
+	def fmt = new SimpleDateFormat("MM/dd/yyyy HH:mm:ss");
+	echo "Date and Time IS: " + fmt.format(date);
     
     if (config.changes != "false"){
     	echo "changes";
