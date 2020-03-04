861a6afbd3c706be6843fd235c1a511f2cb2bd99
Chris Behrens <cbehrens@gmail.com>
2020-03-04 16:23:29 -0600

    Added changeset stuff

--- vars/releasenotes.groovy
+++ vars/releasenotes.groovy
@@ -27,6 +27,20 @@ def call(Map config=[:]){
 	echo "Date and Time IS: " + fmt.format(now);
 	
 	echo "Build Number is: ${BUILD_NUMBER}";
+	
+	def changeLogSets = currentBuild.changeSets
+	for (int i = 0; i < changeLogSets.size(); i++) {
+	    def entries = changeLogSets[i].items
+	    for (int j = 0; j < entries.length; j++) {
+	        def entry = entries[j]
+	        echo "${entry.commitId} by ${entry.author} on ${new Date(entry.timestamp)}: ${entry.msg}"
+	        def files = new ArrayList(entry.affectedFiles)
+	        for (int k = 0; k < files.size(); k++) {
+	            def file = files[k]
+	            echo "  ${file.editType.name} ${file.path}"
+	        }
+	    }
+	}
     
     if (config.changes != "false"){
     	echo "changes";
