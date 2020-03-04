b791a3313c762b52391e7a888e10fb99d0f8f2f3
Chris Behrens <cbehrens@gmail.com>
2020-03-04 16:45:14 -0600

    Refactored for releasenotes file

--- vars/releasenotes.groovy
+++ vars/releasenotes.groovy
@@ -5,44 +5,42 @@ import java.text.SimpleDateFormat
 import hudson.model.*
 
 @NonCPS
-def call(Map config=[:]){
+def call(Map config=[:])
+{
 	def dir = new File(pwd());
-    
+
 	new File(dir.path + '\\releasenotes.txt').withWriter('utf-8') 
 	{ 
 		writer -> 
-	    dir.eachFileRecurse(FileType.ANY){ file ->
-		if (file.isDirectory()){
-		    writer.writeLine(file.name);            
+		
+		def now = new Date();
+		def fmt = new SimpleDateFormat("MM/dd/yyyy HH:mm:ss");
+		writer.writeLine("Date and Time IS: " + fmt.format(now));
+
+		writer.writeLine("Build Number is: ${BUILD_NUMBER}");
+
+		if (config.changes != "false"){
+			def changeLogSets = currentBuild.changeSets;
+
+			for (change in changeLogSets) {
+				def entries = change.items;
+				for (entry in entries) {
+					writer.writeLine("${entry.commitId} by ${entry.author} on ${new Date(entry.timestamp)}: ${entry.msg}");
+					for (file in entry.affectedFiles) {
+						writer.writeLine("${file.editType.name} ${file.path}");
+					}
+				}
+			}
 		}
-		else
-		{
-		    writer.writeLine('\t' + file.name + '\t' + file.length());
-		}
+		
+		dir.eachFileRecurse(FileType.ANY){ file ->
+			if (file.isDirectory()){
+				writer.writeLine(file.name);            
+			}
+			else
+			{
+				writer.writeLine('\t' + file.name + '\t' + file.length());
+			}		
 		} 
 	}
-    
-	def now = new Date();
-	def fmt = new SimpleDateFormat("MM/dd/yyyy HH:mm:ss");
-	echo "Date and Time IS: " + fmt.format(now);
-	
-	echo "Build Number is: ${BUILD_NUMBER}";
-	
-	def changeLogSets = currentBuild.changeSets;
-	
-	echo "There are " + changeLogSets + " changesets.";
-	
-	for (change in changeLogSets) {
-	    def entries = change.items;
-	    for (entry in entries) {
-	        echo "${entry.commitId} by ${entry.author} on ${new Date(entry.timestamp)}: ${entry.msg}"
-	        for (file in entry.affectedFiles) {
-	            echo "  ${file.editType.name} ${file.path}";
-	        }
-	    }
-	}
-    
-    if (config.changes != "false"){
-    	echo "changes";
-    }
 }
