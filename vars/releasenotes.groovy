d68f78211866baef9d6ca8b9d40f304d7b4f3bef
Chris Behrens <cbehrens@gmail.com>
2020-03-04 16:26:55 -0600

    Slimmed down script

--- vars/releasenotes.groovy
+++ vars/releasenotes.groovy
@@ -29,14 +29,11 @@ def call(Map config=[:]){
 	echo "Build Number is: ${BUILD_NUMBER}";
 	
 	def changeLogSets = currentBuild.changeSets
-	for (int i = 0; i < changeLogSets.size(); i++) {
-	    def entries = changeLogSets[i].items
-	    for (int j = 0; j < entries.length; j++) {
-	        def entry = entries[j]
+	for (change in changeLogSets) {
+	    def entries = change.items
+	    for (entry in entries) {
 	        echo "${entry.commitId} by ${entry.author} on ${new Date(entry.timestamp)}: ${entry.msg}"
-	        def files = new ArrayList(entry.affectedFiles)
-	        for (int k = 0; k < files.size(); k++) {
-	            def file = files[k]
+	        for (file in entry.affectedFiles) {
 	            echo "  ${file.editType.name} ${file.path}"
 	        }
 	    }
