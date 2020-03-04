f7ebba357500949231395b670d2d960b4f1690c0
Chris Behrens <cbehrens@gmail.com>
2020-03-04 13:04:25 -0600

    Initial add

---
+++ releasenotes.groovy
@@ -0,0 +1,20 @@
+import java.io.File;
+import groovy.io.FileType;
+
+def call(Map config=[:]){
+    def dir = new File("C:\\Code\\JenkinsGroovy\\ConsoleApp1");
+    // def dir = pwd();
+    
+    new File(dir.path + '\\releasenotes.txt').withWriter('utf-8') { 
+        writer -> 
+            dir.eachFileRecurse(FileType.ANY){ file ->
+                if (file.isDirectory()){
+                    writer.writeLine(file.name);            
+                }
+                else
+                {
+                    writer.writeLine('\t' + file.name + '\t' + file.length());
+                }
+        } 
+    }
+}
