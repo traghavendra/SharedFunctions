ff490ae53d020129b4d3e67be8b9b7f07c37f3b9
Chris Behrens <cbehrens@gmail.com>
2020-03-10 15:42:42 -0500

    Initial add

--- vars/releasenotes.groovy
+++ vars/releasenotes.groovy
@@ -1,47 +1,20 @@
-import groovy.io.FileType;
-import java.io.File;
-import java.util.Calendar.*;
-import java.text.SimpleDateFormat
+import java.io.*;
+import groovy.io.*;
 
-@NonCPS
-def call(Map config=[:])
-{
-	def dir = new File(pwd());
-	
-	echo "Preparing releasenotes.txt at " + dir;
-	
-	new File(dir.path + '/releasenotes.txt').withWriter('utf-8') 
-	{ 
-		writer -> 
-		
-		def now = new Date();
-		def fmt = new SimpleDateFormat("MM/dd/yyyy HH:mm:ss");
-		
-		writer.writeLine("Date and Time IS: " + fmt.format(now));
-		writer.writeLine("Build Number is: ${BUILD_NUMBER}");
-
-		if (config.changes != "false"){
-			def changeLogSets = currentBuild.changeSets;
-
-			for (change in changeLogSets) {
-				def entries = change.items;
-				for (entry in entries) {
-					writer.writeLine("${entry.commitId} by ${entry.author} on ${new Date(entry.timestamp)}: ${entry.msg}");
-					for (file in entry.affectedFiles) {
-						writer.writeLine("${file.editType.name} ${file.path}");
-					}
-				}
-			}
-		}
-		
-		dir.eachFileRecurse(FileType.ANY){ file ->
-			if (file.isDirectory()){
-				writer.writeLine(file.name);            
-			}
-			else
-			{
-				writer.writeLine('\t' + file.name + '\t' + file.length());
-			}		
-		}
-	}
+def call(Map config=[:]){
+    def dir = new File("C:\\Code\\ConsoleApp1"); // pwd();
+    
+    new File(dir.path + '\\releasenotes.txt').withWriter('utf-8') 
+    { 
+    	writer -> 
+                dir.eachFileRecurse(FileType.ANY){ file ->
+    		if (file.isDirectory()){
+    			writer.writeLine(file.name);            
+    		}
+    		else
+    		{
+    			writer.writeLine('\t' + file.name + '\t' + file.length());
+    		}
+           }
+    }
 }
