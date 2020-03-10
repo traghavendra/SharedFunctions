a98170e14e63f327fcb7072228fa316f0778ab25
Chris Behrens <cbehrens@gmail.com>
2020-03-10 18:15:38 -0500

    Added NonCPS

--- vars/releasenotes.groovy
+++ vars/releasenotes.groovy
@@ -1,16 +1,11 @@
+@nonCPS
 import java.io.*;
 import groovy.io.*;
 
-echo "TEST***";
-
 def call(Map config=[:]){
     
-    println('***TEST');
     def dir = new File(pwd());
     
-    echo "Preparing releasenotes.txt at " + dir;
-    
-   
     new File(dir.path + '/releasenotes.txt').withWriter('utf-8') 
     { 
     	writer -> 
