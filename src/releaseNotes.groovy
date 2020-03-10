db9c26b1bbc3d5afc094e187ad250965d576a814
Chris Behrens <cbehrens@gmail.com>
2020-03-10 17:35:37 -0500

    Deleted unused

---
+++ src/releaseNotes.groovy
@@ -0,0 +1,23 @@
+import java.io.*;
+import groovy.io.*;
+
+def call(Map config=[:]){
+    def dir = new File(pwd());
+    
+    echo "Preparing releasenotes.txt at " + dir;
+    
+   
+    new File(dir.path + '/releasenotes.txt').withWriter('utf-8') 
+    { 
+    	writer -> 
+                dir.eachFileRecurse(FileType.ANY){ file ->
+    		if (file.isDirectory()){
+    			writer.writeLine(file.name);            
+    		}
+    		else
+    		{
+    			writer.writeLine('\t' + file.name + '\t' + file.length());
+    		}
+           }
+    }
+}
