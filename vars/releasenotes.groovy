1afcb2d0616376e4ce205c6a025e438dfa652952
Chris Behrens <cbehrens@gmail.com>
2020-03-11 11:08:58 -0500

    Completed

--- vars/releasenotes.groovy
+++ vars/releasenotes.groovy
@@ -20,26 +20,25 @@ def call(Map config=[:]){
     			writer.writeLine('\t' + file.name + '\t' + file.length());
     		}
            }
+    
+        def date = new Date()
+        def sdf = new SimpleDateFormat("MM/dd/yyyy HH:mm:ss")
+        writer.writeLine("Date and Time IS: " + sdf.format(date));
+    
+        writer.writeLine("Build Number is: ${BUILD_NUMBER}");
+        
+        def changeLogSets = currentBuild.changeSets;
+        
+        if (config.changes != "false"){
+            for (change in changeLogSets) {
+            	def entries = change.items;
+            	for (entry in entries) {
+            		writer.writeLine("${entry.commitId} by ${entry.author} on ${new Date(entry.timestamp)}: ${entry.msg}")
+            		for (file in entry.affectedFiles) {
+            			writer.writeLine("${file.editType.name} ${file.path}");
+            		}
+            	}
+            }    
+        }    
     }
-    
-    def date = new Date()
-    def sdf = new SimpleDateFormat("MM/dd/yyyy HH:mm:ss")
-    echo "Date and Time IS: " + sdf.format(date)
-
-    echo "Build Number is: ${BUILD_NUMBER}";
-    
-    def changeLogSets = currentBuild.changeSets;
-    
-    for (change in changeLogSets) {
-    	def entries = change.items;
-    	for (entry in entries) {
-    		echo "${entry.commitId} by ${entry.author} on ${new Date(entry.timestamp)}: ${entry.msg}"
-    		for (file in entry.affectedFiles) {
-    			echo "  ${file.editType.name} ${file.path}";
-    		}
-    	}
-    }    
-    if (config.changes != "false"){
-    	echo "changes";
-    }    
 }
