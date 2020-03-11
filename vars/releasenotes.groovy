9dde32d294bb95358f6ab90fe1681b4debbe34d5
Chris Behrens <cbehrens@gmail.com>
2020-03-11 11:04:57 -0500

    Added changelog

--- vars/releasenotes.groovy
+++ vars/releasenotes.groovy
@@ -2,6 +2,7 @@ import java.io.*;
 import groovy.io.*;
 import java.util.Calendar.*;
 import java.text.SimpleDateFormat
+import hudson.model.*
 
 @NonCPS
 def call(Map config=[:]){
@@ -26,6 +27,18 @@ def call(Map config=[:]){
     echo "Date and Time IS: " + sdf.format(date)
 
     echo "Build Number is: ${BUILD_NUMBER}";
+    
+    def changeLogSets = currentBuild.changeSets;
+    
+    for (change in changeLogSets) {
+    	def entries = change.items;
+    	for (entry in entries) {
+    		echo "${entry.commitId} by ${entry.author} on ${new Date(entry.timestamp)}: ${entry.msg}"
+    		for (file in entry.affectedFiles) {
+    			echo "  ${file.editType.name} ${file.path}";
+    		}
+    	}
+    }    
     if (config.changes != "false"){
     	echo "changes";
     }    
